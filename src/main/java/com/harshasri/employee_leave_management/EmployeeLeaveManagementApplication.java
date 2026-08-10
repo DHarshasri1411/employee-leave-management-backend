ec31c602bf299f71fbea853eae335238f7d637e7
harshasri <harshasri1411@gmail.com>
2026-08-10 17:40:38 +0530

    Fix CORS configuration

--- src/main/java/com/harshasri/employee_leave_management/EmployeeLeaveManagementApplication.java
+++ src/main/java/com/harshasri/employee_leave_management/EmployeeLeaveManagementApplication.java
@@ -3,11 +3,10 @@ package com.harshasri.employee_leave_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.harshasri")
 public class EmployeeLeaveManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeLeaveManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLeaveManagementApplication.class, args);
+    }
 }
--- src/main/java/com/harshasri/employee_leave_management/config/WebConfig.java
+++ src/main/java/com/harshasri/employee_leave_management/config/WebConfig.java
@@ -7,13 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    
-   @Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        System.out.println("========== WEB CONFIG LOADED ==========");
+
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
