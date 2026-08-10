ee045666ecffbd4fe32b357980886c7193f7d741
harshasri <harshasri1411@gmail.com>
2026-08-10 16:10:32 +0530

    Fix CORS for frontend deployment

--- src/main/java/com/harshasri/employee_leave_management/controller/EmployeeController.java
+++ src/main/java/com/harshasri/employee_leave_management/controller/EmployeeController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 
 public class EmployeeController {
