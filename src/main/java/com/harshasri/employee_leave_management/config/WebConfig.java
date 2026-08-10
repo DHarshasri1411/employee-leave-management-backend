2f5e6906e90f42a86c876d3acc18580f065b7900
harshasri <harshasri1411@gmail.com>
2026-08-10 16:37:58 +0530

    Update CORS configuration

--- src/main/java/com/harshasri/employee_leave_management/config/WebConfig.java
+++ src/main/java/com/harshasri/employee_leave_management/config/WebConfig.java
@@ -7,14 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173"
-                        //"https://your-frontend.vercel.app"
-                )
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
+    
+   @Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+}
 }
