d1b2656118c17fe2e1052bb360d304e06e2d9f53
harshasri <harshasri1411@gmail.com>
2026-08-10 19:50:30 +0530

    Fix CORS for Vercel

--- src/main/java/com/harshasri/employee_leave_management/config/WebConfig.java
+++ src/main/java/com/harshasri/employee_leave_management/config/WebConfig.java
@@ -7,15 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+   @Override
+public void addCorsMappings(CorsRegistry registry) {
 
-        System.out.println("========== WEB CONFIG LOADED ==========");
-
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+    registry.addMapping("/**")
+            .allowedOriginPatterns(
+                    "http://localhost:*",
+                    "https://employee-leave-management-frontend-nine.vercel.app",
+                    "https://*.vercel.app"
+            )
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+}
 }
